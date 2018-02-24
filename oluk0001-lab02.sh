aae10c3d94b057dcb8c9283cd9cece8fa9d09bd4
Sam <oluk0001>
2018-02-23 21:41:50 -0600

    2nd script commit

--- oluk0001-lab02.sh
+++ oluk0001-lab02.sh
@@ -4,3 +4,7 @@
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
+mkdir -p ~/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch ~/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get -y install tree
+apt-get -y install git
