4804694c151aba9639c2a668e6ed5c48bbe10816
oluk0001 <oluk0001@github.com>
2018-02-24 02:41:09 +0000

    script commit

---
+++ oluk0001-lab02.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
